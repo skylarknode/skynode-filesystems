767a31b85907227fd77ef2b758490808eddc7296
hudaokeji <github@hudaokeji.com>
2022-04-18 20:57:53 +0900

    update connects/local

--- examples/simple/simple-api-server.js
+++ examples/simple/simple-api-server.js
@@ -11,7 +11,7 @@ const session = require('express-session');
 const Unifile = require('../../lib/index');
 
 const app = express();
-const ufs = new Unifile();
+const volumesRouter = express.Router()
 
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -21,74 +21,79 @@ app.use(session({
 	saveUninitialized: false
 }));
 
+app.use("/volumes",volumesRouter)
+
 // Configure connectors
-const ghconnector = new Unifile.GitHubConnector({
+const ghconnector = new Unifile.connects.GitHubConnector({
 	clientId: 'b4e46028bf36d871f68d',
 	clientSecret: 'c39806c4d0906cfeaac932012996a1919475cc78',
-	redirectUri : "http://localhost:6805/github/oauth-callback"
+	redirectUri : "http://localhost:6805/volumes/github/oauth-callback"
 });
-const dbxconnector = new Unifile.DropboxConnector({
+const dbxconnector = new Unifile.connects.DropboxConnector({
 	clientId: '37mo489tld3rdi2',
 	clientSecret: 'kqfzd11vamre6xr',
-	redirectUri: 'http://localhost:6805/dropbox/oauth-callback'
+	redirectUri: 'http://localhost:6805/volumes/dropbox/oauth-callback'
 });
-const ftpconnector = new Unifile.FtpConnector({redirectUri: 'http://localhost:6805/ftp/signin'});
+const ftpconnector = new Unifile.connects.FtpConnector({redirectUri: 'http://localhost:6805/ftp/signin'});
 ///const wdconnector = new Unifile.WebDavConnector({redirectUri: 'http://localhost:6805/webdav/signin'});
-const rsconnector = new Unifile.HttpConnector({
-	redirectUri: 'http://localhost:6805/http/callback'
+const rsconnector = new Unifile.connects.HttpConnector({
+	redirectUri: 'http://localhost:6805/volumes/http/callback'
 });
-const lsconnector = new Unifile.LocalConnector({
+const lsconnector = new Unifile.connects.LocalConnector({
 	mappedRealFolder : "C:\\tmp",
-	redirectUri: 'http://localhost:6805/local/callback'
+	redirectUri: 'http://localhost:6805/volumes/local/callback'
 });
-const fsconnector = new Unifile.FsConnector({showHiddenFile: true});
-const sftpconnector = new Unifile.SftpConnector({redirectUri: 'http://localhost:6805/sftp/signin'});
+const fsconnector = new Unifile.connects.FsConnector({showHiddenFile: true});
+const sftpconnector = new Unifile.connects.SftpConnector({redirectUri: 'http://localhost:6805/sftp/signin'});
 
 
-const gdriveconnector = new Unifile.GoogleDriveConnector({
+const gdriveconnector = new Unifile.connects.GoogleDriveConnector({
 	clientId: '142047696565-e85s5df5r25tojtk1q1v40eqolc4c9lt.apps.googleusercontent.com',
 	clientSecret: '67Cr51-XRoC2lL_Q9FH5QCIN',
-	redirectUri : "http://localhost:6805/gdrive/oauth-callback"
+	redirectUri : "http://localhost:6805/volumes/gdrive/oauth-callback"
 });
 
-// Register connectors
-ufs.use(ghconnector);
-ufs.use(dbxconnector);
-ufs.use(ftpconnector);
-ufs.use(rsconnector);
-///ufs.use(wdconnector);
-ufs.use(fsconnector);
-ufs.use(lsconnector);
-ufs.use(sftpconnector);
-ufs.use(gdriveconnector);
+const volumes = new Unifile.connects.VolumesConnector({
+});
 
+
+// mount volumes
+volumes.mount(ghconnector);
+volumes.mount(dbxconnector);
+volumes.mount(ftpconnector);
+volumes.mount(rsconnector);
+///ufs.use(wdconnector);
+volumes.mount(fsconnector);
+volumes.mount(lsconnector);
+volumes.mount(sftpconnector);
+volumes.mount(gdriveconnector);
+
+const ufs = new Unifile.FileSystem(volumes);
 
 // Search for a old session token in the cookies
-app.get('/', function(req, res) {
+volumesRouter.get('/', function(req, res) {
 	// Init ufs session in Express
 	req.session.ufs = req.session.ufs || {};
 
 	let response;
 	if(req.cookies.ufs_github)
-		response = ufs.setAccessToken(req.session.ufs, 'github', req.cookies.ufs_github);
+		response = ufs.setAccessToken(req.session.ufs, req.cookies.ufs_github, 'github');
 	if(req.cookies.ufs_dropbox)
-		response = ufs.setAccessToken(req.session.ufs, 'dropbox', req.cookies.ufs_dropbox);
+		response = ufs.setAccessToken(req.session.ufs, req.cookies.ufs_dropbox, 'dropbox');
 
 	if(response)
-		response.then(() => res.sendFile(Path.join(__dirname, 'public', 'index.html')));
-	else res.sendFile(Path.join(__dirname, 'public', 'index.html'));
+		response.then(() => res.sendFile(Path.join(__dirname, 'public/volumes', 'index.html')));
+	else res.sendFile(Path.join(__dirname, 'public/volumes', 'index.html'));
 });
 
 
-require("../../lib/routes/authorizes")(app,ufs,{
-	signin : function(req, res) {
-      res.sendFile(Path.join(__dirname, 'public', req.params.connector + '_login.html'));
-    }
+require("../../lib/routes/volumes")(volumesRouter,ufs,{
+	"authorizes" : {
+		signin : function(req, res) {
+	      res.sendFile(Path.join(__dirname, 'public/volumes', req.params.connector + '_login.html'));
+	    }
+	}
 });
-
-require("../../lib/routes/singulars")(app,ufs);
-
-require("../../lib/routes/batch")(app,ufs);
 
 
 // server 'loop'
